72e2ee4a96364d1b1b34d7e122e5d092a992a47f
nghiatmp <leminhnghia080198@gmail.com>
2020-03-23 22:52:47 +0700

    add phase

--- resources/js/components/projectDetail/phase.vue
+++ resources/js/components/projectDetail/phase.vue
@@ -31,7 +31,7 @@
                             <v-layout justify-center>
                                 <v-icon @click="getInForPhase(item)">info</v-icon>
                                 <v-icon class="ml-2">fas fa-edit</v-icon>
-                                <v-icon class="ml-2">far fa-clone</v-icon>
+                                <v-icon class="ml-2" @click="diaLogChangeStatus=true">far fa-clone</v-icon>
                             </v-layout>
                         </template>
                     </v-data-table>
@@ -171,11 +171,107 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
+        <v-dialog
+            v-model="diaLogChangeStatus"
+            width="700px"
+            height="600px"
+        >
+            <v-card>
+                <v-card-title>
+                    Create Project
+                </v-card-title>
+                <v-container>
+                    <v-row class="mx-2">
+                        <v-col class="align-center" cols="6">
+                            <v-select
+                                v-model="paramChangeStatus.status"
+                                :items="StatusPhase"
+                                label="Status"
+                                item-value="key"
+                                item-text="value"
+                                :hide-details="true"
+                                dense
+                                outlined
+                                required
+                                :error-messages="StatusChangeErrors"
+                                @change="$v.paramChangeStatus.status.$touch()"
+                                @blur="$v.paramChangeStatus.status.$touch()"
+                            />
+                        </v-col>
+                        <v-col class="align-center justify-space-between" cols="6">
+                            <v-text-field
+                                v-model="paramChangeStatus.css"
+                                label="Css Point"
+                                dense
+                                outlined
+                                required
+                                :error-messages="CssChangeErrors"
+                                @change="$v.paramChangeStatus.css.$touch()"
+                                @blur="$v.paramChangeStatus.css.$touch()"
+                            />
+<!--                            <div class="v-text-field__details" v-if="errExistKeyCreate">-->
+<!--                                <div class="v-messages theme&#45;&#45;light error&#45;&#45;text" role="alert">-->
+<!--                                    <div class="v-messages__wrapper">-->
+<!--                                        <div class="v-messages__message">This key has already been set for another project</div>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+                        </v-col>
+                        <v-col class="align-center justify-space-between" cols="6">
+                            <v-text-field
+                                v-model="paramChangeStatus.leakage"
+                                label="Leakage"
+                                dense
+                                outlined
+                                required
+                                :error-messages="LeakageChangeErrors"
+                                @change="$v.paramChangeStatus.leakage.$touch()"
+                                @blur="$v.paramChangeStatus.leakage.$touch()"
+                            />
+                        </v-col>
+                        <v-col class="align-center justify-space-between" cols="6">
+                            <v-text-field
+                                v-model="paramChangeStatus.ee"
+                                label="EE"
+                                dense
+                                outlined
+                                required
+                                :error-messages="EEChangeErrors"
+                                @change="$v.paramChangeStatus.ee.$touch()"
+                                @blur="$v.paramChangeStatus.ee.$touch()"
+                            />
+                        </v-col>
+                        <v-col class="align-center justify-space-between" cols="12">
+                            <v-text-field
+                                v-model="paramChangeStatus.timeliness"
+                                label="Timeliness"
+                                dense
+                                outlined
+                                required
+                                :error-messages="TimeLinessChangeErrors"
+                                @change="$v.paramChangeStatus.timeliness.$touch()"
+                                @blur="$v.paramChangeStatus.timeliness.$touch()"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-card-actions>
+                    <v-btn
+                        @click="ClearValidateChangeStatus"
+                    >Cancel</v-btn>
+                    <v-btn
+                        color="primary"
+                        @click="changeStatusPhase"
+                    >Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-card>
 </template>
 
 <script>
     import {PHASE_STATUS} from "../../constants/common";
+    import {between, integer, required, minValue, numeric} from "vuelidate/lib/validators";
     export default {
         data () {
             return {
@@ -189,6 +285,7 @@
                 isLoading:false,
                 diaLogcreateMember:false,
                 diaLogInForPhase:false,
+                diaLogChangeStatus:false,
                 addToResource:false,
                 paramCreate: {
                     'user_id':'',
@@ -197,14 +294,17 @@
                     'from_at':'',
                     'to_at':'',
                 },
+                paramChangeStatus: {
+                    'status':'',
+                    'css':'',
+                    'leakage':'',
+                    'ee':'',
+                    'timeliness':'',
+                },
+                StatusPhase:[],
                 'UserCreate' :[],
                 'RoleCreate' : [],
-                desserts: [
-                    {
-                        name: 'Frozen Yogurt',
-                        value: 159,
-                    },
-                ],
+                desserts: [],
                 headers: [
                     { text: 'Phase', value: 'title' },
                     { text: 'Start', value: 'from_at' },
@@ -221,6 +321,53 @@
             ProjectID(){
                 return this.$route.params.proID;
             },
+            StatusChangeErrors () {
+                const errors = [];
+                if (!this.$v.paramChangeStatus.status.$dirty) return errors;
+                !this.$v.paramChangeStatus.status.required && errors.push('Status is required.');
+                return errors
+            },
+            CssChangeErrors () {
+                const errors = [];
+                if (!this.$v.paramChangeStatus.css.$dirty) return errors;
+                !this.$v.paramChangeStatus.css.required && errors.push('Css is required.');
+                !this.$v.paramChangeStatus.css.numeric && errors.push('Css is numeric.');
+                !this.$v.paramChangeStatus.css.between && errors.push('Css is between 0% and 100%.');
+                return errors
+            },
+            LeakageChangeErrors () {
+                const errors = [];
+                if (!this.$v.paramChangeStatus.leakage.$dirty) return errors;
+                !this.$v.paramChangeStatus.leakage.required && errors.push('Leakage is required.');
+                !this.$v.paramChangeStatus.leakage.numeric && errors.push('Leakage is numeric.');
+                !this.$v.paramChangeStatus.leakage.minValue && errors.push('Please enter a value greater than or equal to 0');
+                return errors
+            },
+            EEChangeErrors () {
+                const errors = [];
+                if (!this.$v.paramChangeStatus.ee.$dirty) return errors;
+                !this.$v.paramChangeStatus.ee.required && errors.push('EE is required.');
+                !this.$v.paramChangeStatus.ee.numeric && errors.push('EE is numeric.');
+                !this.$v.paramChangeStatus.ee.minValue && errors.push('Please enter a value greater than or equal to 0');
+                return errors
+            },
+            TimeLinessChangeErrors () {
+                const errors = [];
+                if (!this.$v.paramChangeStatus.timeliness.$dirty) return errors;
+                !this.$v.paramChangeStatus.timeliness.required && errors.push('Css is required.');
+                !this.$v.paramChangeStatus.timeliness.numeric && errors.push('Css is numeric.');
+                !this.$v.paramChangeStatus.timeliness.between && errors.push('Css is between 0% and 100%.');
+                return errors
+            },
+        },
+        validations : {
+            paramChangeStatus: {
+                status: { required},
+                css: {required, numeric, between: between(0, 100)},
+                leakage: {required, numeric, minValue : 0},
+                ee: {required, numeric, minValue : 0},
+                timeliness: {required, numeric, between: between(0, 100)},
+            }
         },
         created(){
             this.renderData();
@@ -246,8 +393,52 @@
             },
             getInForPhase(item) {
                 this.diaLogInForPhase=true;
-                const dataReturn= [];
-            }
+                this.desserts= [
+                    {
+                        name: 'Name',
+                        value:item.title
+                    }, {
+                        name: 'Status',
+                        value:item.status
+                    }, {
+                        name: 'From',
+                        value:item.from_at
+                    }, {
+                        name: 'To',
+                        value:item.to_at
+                    }, {
+                        name: 'Effort (MM)',
+                        value:item.used_effort
+                    },{
+                        name: 'Budget',
+                        value:item.budget
+                    },{
+                        name: 'CSS Point',
+                        value:item.css
+                    },{
+                        name: 'Timeliness (%)',
+                        value: item.timeliness
+                    }, {
+                        name: 'EE (%)',
+                        value:item.ee
+                    }
+                ];
+            },
+            changeStatusPhase(){
+                this.$v.diaLogChangeStatus.$touch();
+                if (!this.$v.diaLogChangeStatus.$invalid) {
+                    console.log('ok');
+                }
+            },
+            ClearValidateChangeStatus() {
+                this.diaLogChangeStatus=false;
+                this.$v.paramChangeStatus.$reset();
+                this.paramChangeStatus.status='';
+                this.paramChangeStatus.css='';
+                this.paramChangeStatus.leakage='';
+                this.paramChangeStatus.ee='';
+                this.paramChangeStatus.timeliness='';
+            },
         }
     }
 </script>
